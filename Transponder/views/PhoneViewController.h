77fbb223f77b9a221bf1400cbe5c8bcef85151c1
Jack Cook <jack@jackcook.nyc>
2015-01-24 14:24:51 -0800

    Create PhoneViewController

--- Transponder/views/PhoneViewController.h
+++ Transponder/views/PhoneViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PhoneViewController.h
 //  Transponder
 //
 //  Created by Jack Cook on 1/24/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PhoneViewController : UIViewController
 
 @end
-
---
+++ Transponder/views/PhoneViewController.m
@@ -0,0 +1,21 @@
+//
+//  PhoneViewController.m
+//  Transponder
+//
+//  Created by Jack Cook on 1/24/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+#import "PhoneViewController.h"
+
+@interface PhoneViewController ()
+
+@end
+
+@implementation PhoneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
