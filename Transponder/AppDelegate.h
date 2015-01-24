bce8c1094615ac735f6162ceac64134a0f9a326d
Jack Cook <jack@jackcook.nyc>
2015-01-24 13:30:37 -0800

    Initial Commit

---
+++ Transponder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transponder
+//
+//  Created by Jack Cook on 1/24/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Transponder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Transponder
+//
+//  Created by Jack Cook on 1/24/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
